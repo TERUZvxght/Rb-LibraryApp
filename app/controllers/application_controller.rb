2d16e4f5903fe720289812c769adcc07cda739c0
TERUZvxght <terunori_kato@icloud.com>
2025-09-15 14:38:31 +0900

    Adminの場合にのみBooksにNewBook、LoansにNewLoanを表示するように変更

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,11 +1,16 @@
 class ApplicationController < ActionController::Base
   # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
   allow_browser versions: :modern
+  helper_method :is_admin?
 
   private
     def require_admin
       unless current_user&.admin?
         redirect_to books_path, alert: "管理者権限が必要です。"
       end
+    end
+
+    def is_admin?
+      current_user&.admin?
     end
 end
